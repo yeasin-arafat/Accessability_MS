ea4743722320c615438835f2fb12e00f5fecb70c
yeasin-arafat <rafan.arafat07@gmail.com>
2017-07-07 11:37:03 +0600

    Accessability_MS project created

---
+++ src/main/java/com/bhm/accessability/AccessabilityApplication.java
@@ -0,0 +1,12 @@
+package com.bhm.accessability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessabilityApplication.class, args);
+	}
+}
